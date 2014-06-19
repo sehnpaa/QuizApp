f21b35ec52e6d6abd97266bcd0cdb49dd97c42bf
n4sa <jacobtipp@gmail.com>
2014-06-19 01:23:56 -0700

    updated index.html and logic.js

--- js/logic.js
+++ js/logic.js
@@ -1,127 +1,135 @@
-!function (obj) {	
-	var create = Model.data,
-		quiz = $('#quiz'),
-		next = $('#quizWrapper'),
-		nextButton = $("input[type='submit']"),
-		back = $('button'),
-		template = $('#questionTemp').innerHTML,
-		lastTemp = $('#finished').innerHTML,
-		currentPage,
-		leng = data.length,
-		local,
-		input,
-		views;
+  !function (obj) {
+    var create = Model.data,
+        quiz = $('#quiz'),
+        next = $('#quizWrapper'),
+        nextButton = $("input[type='submit']"),
+        back = $('button'),
+        restart = $('#restart'),
+        template = $('#questionTemp').innerHTML,
+        lastTemp = $('#finished').innerHTML,
+        leng = data.length,
+        currentPage,
+        local,
+        input,
+        views;
 
-	function createViews () {
-	 	views = data.map(function (val) {
-			return new create(val);
-		});
-		animate(next, 'slideInDown');
+    function createViews() {
+        views = data.map(function (val) {
+            return new create(val);
+        });
+        animate(next, 'slideInDown');
 
-		if(!store.getData('user')) {
-			back.hide();
-	 		store.setData('user', {currentPage: 0, userAnswer: []});
-	 		local = store.getData('user');
-	 		currentPage = 0;
-	 		quiz.innerHTML = views[currentPage].renderTemplate(template);
+        if (!store.getData('user')) {
+            back.hide();
+            store.setData('user', {
+                currentPage: 0,
+                userAnswer: []
+            });
+            local = store.getData('user');
+            currentPage = 0;
+            quiz.innerHTML = views[currentPage].renderTemplate(template);
 
-	 	} else {
-	 		local = store.getData('user');
-	 		currentPage = local.currentPage;
+        } else {
+            local = store.getData('user');
+            currentPage = local.currentPage;
 
-	 		if(currentPage === (data.length)) 
-	 			return finish();
-	 		
-	 		if(!currentPage)
-	 			back.hide();
-	 		else 
-	 			back.show();
+            if (currentPage === (data.length)) return finish();
 
-	 		quiz.innerHTML = views[currentPage].renderTemplate(template);
-	 		checkAnswer();
-	 	}
-	 }
+            if (!currentPage) back.hide();
+            else back.show();
 
-	 function nextPage(event) {
-	 	event.preventDefault();
-	 	animate(next, 'slideOutRight');
-	 	window.setTimeout(function () {
+            restart.hide();
+            quiz.innerHTML = views[currentPage].renderTemplate(template);
+            checkAnswer();
+        }
+    }
 
-			isAnswered();
-	 		animate(next, 'slideInLeft');
-		 	currentPage++;
-		 	updateStore(currentPage);
-		 	if(currentPage === (data.length))
-		 		return finish();
-		 	if(currentPage)
-		 		back.show();
-		 	quiz.innerHTML = views[currentPage].renderTemplate(template);
-		 	checkAnswer();
-		 	}, 1000);
-	 }
+    function nextPage(event) {
+        event.preventDefault();
+        animate(next, 'slideOutRight');
+        window.setTimeout(function () {
 
-	 function isAnswered () {
-	    input = $('input');
-	 	for(var i = 0; i < input.length; i++) {
-	 		if(input[i].checked){
-	 			local.userAnswer[currentPage] = input[i].value;
-	 		}
-	 	}
-	 }
+            isAnswered();
+            animate(next, 'slideInLeft');
+            currentPage++;
+            updateStore(currentPage);
+
+            if (currentPage === (data.length)) return finish();
+
+            if (currentPage) back.show();
+
+            quiz.innerHTML = views[currentPage].renderTemplate(template);
+            checkAnswer();
+        }, 1000);
+    }
+
+    function isAnswered() {
+        input = $('input');
+        for (var i = 0; i < input.length; i++) {
+            if (input[i].checked) {
+                local.userAnswer[currentPage] = input[i].value;
+            }
+        }
+    }
 
 
- 	 function updateStore (count) {
- 		local.currentPage = count;
- 		store.setData('user', local);
- 	}
+    function updateStore(count) {
+        local.currentPage = count;
+        store.setData('user', local);
+    }
 
- 	function checkAnswer () {
- 		input = $('input');
- 		for(var i = 0; i < input.length; i++) {
- 			if(input[i].value === local.userAnswer[currentPage])
- 				input[i].checked = true;
- 		}
- 	}
+    function checkAnswer() {
+        input = $('input');
+        for (var i = 0; i < input.length; i++) {
+            if (input[i].value === local.userAnswer[currentPage]) input[i].checked = true;
+        }
+    }
 
- 	function finish () {
- 		var answers = data.map(function (val) {
- 			return val.answer;
- 		}),
- 			correct = answers.compare(local.userAnswer);
- 			quiz.innerHTML = new Model.finished(correct, data.length).render(lastTemp);
- 			nextButton.hide();
- 	}
+    function finish() {
+        var answers = data.map(function (val) {
+            return val.answer;
+        }),
+            correct = answers.compare(local.userAnswer);
+        quiz.innerHTML = new Model.finished(correct, data.length).render(lastTemp);
+        nextButton.hide();
+        restart.show();
+    }
 
- 	function animate (id, animation) {
-		id.className = 'animated ' + animation ;
-	 	window.setTimeout(function () {
-	 		id.className = '';
-	 	}, 1000);
-	 }
+    function animate(id, animation) {
+        id.className = 'animated ' + animation;
+        window.setTimeout(function () {
+            id.className = '';
+        }, 1000);
+    }
 
- 	function $ (query) {
- 		var selector = document.querySelectorAll(query);
- 		return selector.length > 1 ? selector : selector[0];
-	}
+    function $(query) {
+        var selector = document.querySelectorAll(query);
+        return selector.length > 1 ? selector : selector[0];
+    }
 
-	function goBack () {
-		animate(next, 'slideOutLeft');
-	 	window.setTimeout(function () {
-	 		isAnswered();
-	 		animate(next, 'slideInRight');
-			currentPage--;
-			updateStore(currentPage);
-			nextButton.show();
+    function goBack() {
+        animate(next, 'slideOutLeft');
+        window.setTimeout(function () {
+            isAnswered();
+            animate(next, 'slideInRight');
+            currentPage--;
+            updateStore(currentPage);
+            nextButton.show();
 
-			if(!currentPage)
-				back.hide();
+            if (!currentPage) back.hide();
 
-			quiz.innerHTML = views[currentPage].renderTemplate(template);
-			checkAnswer();
-		},1000);
-	}
+            quiz.innerHTML = views[currentPage].renderTemplate(template);
+            checkAnswer();
+        }, 1000);
+    }
 
-window.addEventListener('load', createViews);
-next.addEventListener('submit', nextPage);
-back.addEventListener('click', goBack);
+    function restartQuiz () {
+    	localStorage.clear();
+    	createViews();
+    }
+
+    window.addEventListener('load', createViews);
+    next.addEventListener('submit', nextPage);
+    back.addEventListener('click', goBack);
+    restart.addEventListener('click', restartQuiz);
 }(window.Model);
