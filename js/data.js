5577c284447b2b7e949c5badee39fe73ac8a1143
n4sa <jacobtipp@gmail.com>
2014-06-19 00:39:26 -0700

    added js folder modified index.html and logic.js

---
+++ js/data.js
@@ -0,0 +1,44 @@
+!function () {
+
+    return window.data = [{
+        "question": "What is the capital of New Hampshire?",
+        "choices": ["Memphis", "Concord", "Manchester", "San Franciso"],
+        "answer": "Concord"
+    }, {
+        "question": "What color are frogs?",
+        "choices": ["Green", "Blue", "Orange", "Yellow"],
+        "answer": "Green"
+    }, {
+        "question": "How many planets are there?",
+        "choices": ["7", "9", "8", "10"],
+        "answer": "8"
+    }, {
+        "question": "If there are five apples, and you take away three, how many do you have?",
+        "choices": ["0", "1", "2", "3"],
+        "answer": "2"
+    }, {
+        "question": "In baseball, how many outs are there in an inning?",
+        "choices": ["1", "3", "6", "9"],
+        "answer": "3"
+    }, {
+        "question": "A rancher has 33 head of cattle standing in a field, when suddenly a bolt of lightning kills all but 9 of them. How many head of cattle are left standing?",
+        "choices": ["24", "9", "4", "22"],
+        "answer": "9"
+    }, {
+        "question": "What is Amazon's reading device called?",
+        "choices": ["Fiddle", "Kinling", "Bindle", "Kindle"],
+        "answer": "Kindle"
+    }, {
+        "question": "If the hour hand on a clock moves 1/60 of a degree every minute how many degrees will the hand move in one hour?",
+        "choices": ["0", "30", "60", "15"],
+        "answer": "60"
+    }, {
+        "question": "Some months have 31 days, and some months have 30 days. But how many have 28 days? ",
+        "choices": ["1", "12", "3", "4"],
+        "answer": "12"
+    }, {
+        "question": "John digs a hole that is 2 yards wide, 3 yards long, and 1 yard deep. How many cubic yards is the hole?",
+        "choices": ["3", "6", "5", "2"],
+        "answer": "6"
+    }];
+}();
